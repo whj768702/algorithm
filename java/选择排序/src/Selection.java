1079668af5f361cb77005dc999104fa6b93e29cf
whj768702 <whj768702@gmail.com>
2018-12-17 10:31:46 +0800

    选择排序。

---
+++ java/选择排序/src/Selection.java
@@ -0,0 +1,49 @@
+/**
+ * @author w
+ */
+public class Selection {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exchange(a, i, min);
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Comparable[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        sort(array);
+        assert isSorted(array);
+        show(array);
+    }
+}
